0d75c09b8a31db86f888724fead3392766a437b8
NachoJ12 <nacho.justel12@gmail.com>
2021-11-18 18:42:27 -0300

    Agrego compareTo en la main a traves de un condicional para dar salida con println

--- claseCatorce_embarcacion/src/com/company/Main.java
+++ claseCatorce_embarcacion/src/com/company/Main.java
@@ -12,12 +12,23 @@ public class Main {
         EmbarcacionYate yate2 = new EmbarcacionYate(capitan3, 110000.00, 12000.0,2020, 70.40, 53);
         EmbarcacionVelero velero1 = new EmbarcacionVelero(capitan2, 40000.50, 9557.40, 2021, 27.92, 2);
 
-        System.out.println(yate1.calcularAlquiler());
-        System.out.println(yate1.compareTo(yate2));
-
         System.out.println(velero1.esGrande());
         System.out.println(velero1.calcularAlquiler());
+        System.out.println("\n");
+
+        System.out.println(yate1.calcularAlquiler());
+        System.out.println(yate1.compareTo(yate2));
+        System.out.println("\n");
 
         yate2.capitanDeEmbarcacion();
+        System.out.println("\n");
+
+        if(yate1.compareTo(yate2) > 0){
+            System.out.println("Yate 1 tiene más camarotes que Yate 2");
+        } else if(yate1.compareTo(yate2) < 0){
+            System.out.println("Yate 2 tiene más camarotes que Yate 1");
+        } else {
+            System.out.println("Yate 1 tiene los mismos camarotes que Yate 2");
+        }
     }
 }
