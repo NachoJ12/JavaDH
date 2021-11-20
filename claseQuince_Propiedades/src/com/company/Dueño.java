b9b692f57661930c750cee89b3ef73e7f9b79eb5
NachoJ12 <nacho.justel12@gmail.com>
2021-11-20 00:07:15 -0300

    actualizo nombre proyecto

---
+++ claseQuince_Propiedades/src/com/company/Dueño.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Dueño {
+    private String nombre;
+    private String apellido;
+    private String cuil;
+
+    public Dueño(String nombre, String apellido, String cuil) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.cuil = cuil;
+    }
+
+}
